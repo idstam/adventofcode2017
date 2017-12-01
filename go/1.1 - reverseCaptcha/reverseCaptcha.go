7413bd4694e9b634992232284712d643271172cd
Johan Idstam <johan.idstam@adlibris.com>
2017-12-01 08:49:51 +0100

    Succeeded 1.1

--- go/1.1 - reverseCaptcha/reverseCaptcha.go
+++ go/1.1 - reverseCaptcha/reverseCaptcha.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -10,5 +11,17 @@ func main() {
 
 	var foo = captcha + captcha[0:1]
 
-	fmt.Println(foo)
+	i := 0
+	sum := 0
+	for i < len(foo)-1 {
+		a := foo[i : i+1]
+		b := foo[i+1 : i+2]
+		if a == b {
+			loopVal, _ := strconv.Atoi(foo[i : i+1])
+			sum = sum + loopVal
+
+		}
+		i = i + 1
+	}
+	fmt.Println(sum)
 }
