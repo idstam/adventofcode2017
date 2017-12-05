47edd34bc5e55075bb1beed911ba656fe2fa3f60
Johan Idstam <johan@idstam.com>
2017-12-05 07:08:07 +0100

    Day 5.1

---
+++ go/05.1 A Maze of Twisty Trampolines/maze.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	memory := []int{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		word, _ := strconv.Atoi(scanner.Text())
+		memory = append(memory, word)
+	}
+
+	address := 0
+	ctr := 0
+	for {
+		ctr++
+		word := memory[address]
+		memory[address] = memory[address] + 1
+		address += word
+		if address >= len(memory) {
+			break
+		}
+	}
+
+	fmt.Println(ctr)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
