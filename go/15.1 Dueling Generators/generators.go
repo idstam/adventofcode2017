4fb03373d8f1d4d3f82e91257d4dc12a549c8c4d
johan idstam <johan@idstam.com>
2018-01-15 07:10:30 +0100

    Solved part two by editing the same file

--- go/15.1 Dueling Generators/generators.go
+++ go/15.1 Dueling Generators/generators.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	//inA := 699
-	//inB := 124
+	inA := 699
+	inB := 124
 
-	inA := 65
-	inB := 8921
+	//inA := 65
+	//inB := 8921
 	eqCount := 0
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 5000000; i++ {
 		inA = generatorA(inA)
 		inB = generatorB(inB)
 
-		fmt.Printf("%d %d \n", inA, inB)
+		//fmt.Printf("%d %d \n", inA, inB)
 		eq := true
 		mask := 1
 
@@ -42,25 +42,25 @@ func main() {
 
 func generatorA(input int) int {
 	for {
-		ret := input * 16807
-		foo := ret % 2147483647
-		foo2 := foo % 4
-		if foo2 == 0 {
-			return ret
+		prd := input * 16807
+		rem := prd % 2147483647
+		doReturn := rem % 4
+		if doReturn == 0 {
+			return rem
 		}
-		input = ret
+		input = rem
 	}
 }
 
 func generatorB(input int) int {
 	for {
-		ret := input * 48271
-		foo := ret % 2147483647
-		foo2 := foo % 8
-		if foo2 == 0 {
-			return ret
+		prd := input * 48271
+		rem := prd % 2147483647
+		doReturn := rem % 8
+		if doReturn == 0 {
+			return rem
 		}
-		input = ret
+		input = rem
 
 	}
 }
