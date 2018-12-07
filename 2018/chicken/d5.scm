76ea7f0bb4500c246900799cafdd46193cb1ff0b
johan idstam <johan@idstam.com>
2018-12-07 07:51:15 +0100

    wip

--- 2018/chicken/d5.scm
+++ 2018/chicken/d5.scm
@@ -1,21 +1,64 @@
 (use utils)
+(require-extension list-utils)
 (use srfi-1)
 (use srfi-13)
 (use srfi-69)
 (use vector-lib)
 
 
-(define (build-remove-map line removedMap index)
-    (if (= index (- (string-length line) 1 ))
-        removedMap
-        (begin
+(define (delete-n l n)
+  (if (= n 0) 
+      (cdr l)
+      (append (list (car l)) (delete-n (cdr l) (- n 1))))
+)
 
+
+(define (poly-pair? a b)
+    (print (list a b ) (= 32 (abs (- a b) )))
+    (= 32 (abs (- a b) ))
+)
+
+(define (find-first-poly-pair int-list i)
+    (if (length=1? int-list )
+        #f
+        (begin
+            (define a (first int-list))
+            (define b (second int-list))
+            (if (poly-pair? a b)
+                i
+                (find-first-poly-pair (cdr int-list) (+ 1 i))
+            )
         )
     )
 )
+
+
+(define (filter-poly-pairs int-list i)
+    (if (= (length int-list) (+ 1 i))
+        int-list
+        (begin
+            (define a (first int-list))
+            (define b (second int-list))
+            (if (poly-pair? a b)
+                (begin 
+                    (define a laksflkasdjfklasdjfas)asdfasdfasdfasdf
+                    (define la (delete-n int-list a))
+                    (define lb (delete-n la (+ 1 a)))
+                    (filter-poly-pairs lb 0)
+                )
+                (filter-poly-pairs int-list (+ 1 i))
+            )
+        )
+    )
+)
+
+
 (define (main args)
-    ;(define a (procLines 2 (read-lines "d5data.txt") 0 ))
-    (define removedMap (build-remove-map "dabAcCaCBAcCcaDA" (make-hash-table) 0)
-    (print (hash-table-keys removedMap))
-	
+    (define int-list (map-in-order char->integer (string->list (car (read-lines "d5data.txt")))))
+    (print int-list)
+    (define filtered-list (filter-poly-pairs int-list 0))
+    (print filtered-list)
+    ;(print (find-first-poly-pair int-list 0))
+    (print (list->string (map-in-order integer->char filtered-list)))
+
 )
