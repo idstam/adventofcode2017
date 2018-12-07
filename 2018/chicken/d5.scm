c955d23ac04c4c04061411b0aa4b0e19bde5b071
johan.idstam <johan.idstam@adlibris.com>
2018-12-07 15:58:28 +0100

    ..

--- 2018/chicken/d5.scm
+++ 2018/chicken/d5.scm
@@ -12,9 +12,14 @@
       (append (list (car l)) (delete-n (cdr l) (- n 1))))
 )
 
+(define (nth l n)
+  (if (or (> n (length l)) (< n 0))
+    (error "Index out of bounds.")
+    (if (eq? n 0)
+      (car l)
+      (nth (cdr l) (- n 1)))))
 
 (define (poly-pair? a b)
-    (print (list a b ) (= 32 (abs (- a b) )))
     (= 32 (abs (- a b) ))
 )
 
@@ -32,18 +37,16 @@
     )
 )
 
-
 (define (filter-poly-pairs int-list i)
     (if (= (length int-list) (+ 1 i))
         int-list
         (begin
-            (define a (first int-list))
-            (define b (second int-list))
+            (define a (nth int-list i))
+            (define b (nth int-list (+ 1 i)))
             (if (poly-pair? a b)
                 (begin 
-                    (define a laksflkasdjfklasdjfas)asdfasdfasdfasdf
-                    (define la (delete-n int-list a))
-                    (define lb (delete-n la (+ 1 a)))
+                    (define la (delete-n int-list i))
+                    (define lb (delete-n la i)) ;Eftersom jag tog bort en precis är det samma index som ska tas bort igen
                     (filter-poly-pairs lb 0)
                 )
                 (filter-poly-pairs int-list (+ 1 i))
@@ -55,10 +58,8 @@
 
 (define (main args)
     (define int-list (map-in-order char->integer (string->list (car (read-lines "d5data.txt")))))
-    (print int-list)
     (define filtered-list (filter-poly-pairs int-list 0))
-    (print filtered-list)
-    ;(print (find-first-poly-pair int-list 0))
-    (print (list->string (map-in-order integer->char filtered-list)))
-
+    (define polymer (list->string (map-in-order integer->char filtered-list)))
+    (print polymer)
+    (print (string-length polymer))
 )
