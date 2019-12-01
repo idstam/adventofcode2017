99059bf4e3ae52e371a4f945605038eada31862d
Johan Idstam <johan@idstam.com>
2019-12-01 08:26:15 +0100

    2019 Day 1.2

--- 2019/go/01.1 Rocket Equation/main.go
+++ 2019/go/01.1 Rocket Equation/main.go
@@ -3,11 +3,34 @@ package main
 import "fmt"
 
 func main() {
+
 	lines := fileToLines("input.txt")
 
 	ints := StringToIntArray(lines)
 	ints = DivideByOnIntArray(ints, 3)
 	ints = SubtractOnIntArray(ints, 2)
+	fuels := ExtraFuelArray(ints)
+
+	sumFuel := SumIntArray(fuels)
 	sum := SumIntArray(ints)
-	fmt.Println(sum)
+	fmt.Printf("Fule requirement for modules: %d\n", sum)
+	fmt.Printf("Fule requirement for modules and it's fuel: %d\n", sum+sumFuel)
+}
+
+func ExtraFuelArray(in []int) []int {
+	ret := []int{}
+	for _, i := range in {
+		ret = append(ret, ExtraFuel(i))
+	}
+	return ret
+}
+func ExtraFuel(mass int) int {
+	f := (mass / 3) - 2
+	ret := mass
+	for f > 0 {
+		ret += f
+		f = (f / 3) - 2
+	}
+
+	return ret - mass
 }
