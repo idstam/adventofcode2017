bdebc0c115ad9af0389b10745ecfe1a43a52fdf1
Johan Idstam <johan@idstam.com>
2019-12-02 19:42:47 +0100

    2019 Day 2 Part 2

--- 2019/go/02.1 1202 Program/main.go
+++ 2019/go/02.1 1202 Program/main.go
@@ -9,26 +9,36 @@ var mem []int
 func main() {
 	lines := fileToLines("input.txt")
 	strs := strings.Split(lines[0], ",")
-	mem = StringToIntArray(strs)
 
-	mem[1] = 12
-	mem[2] = 2
-	ptr := 0
-	for ptr >= 0 {
-		ptr = Exec(ptr)
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			mem = StringToIntArray(strs)
 
+			mem[1] = noun
+			mem[2] = verb
+			ptr := 0
+			for ptr >= 0 {
+				ptr = Exec(ptr)
+
+			}
+
+			if mem[0] == 19690720 {
+				fmt.Printf("Noun %d  Verb %d  Answer %d\n", noun, verb, (100*noun)+verb)
+			}
+		}
 	}
-
 	//Too low: 29891
-	fmt.Println(mem[0])
+	//fmt.Println(mem[0])
 }
 
 func Exec(ptr int) int {
 	switch mem[ptr] {
 	case 1:
 		mem[mem[ptr+3]] = mem[mem[ptr+1]] + mem[mem[ptr+2]]
+		ptr += 4
 	case 2:
 		mem[mem[ptr+3]] = mem[mem[ptr+1]] * mem[mem[ptr+2]]
+		ptr += 4
 	case 99:
 		return -1
 	default:
@@ -36,5 +46,5 @@ func Exec(ptr int) int {
 
 	}
 
-	return ptr + 4
+	return ptr
 }
