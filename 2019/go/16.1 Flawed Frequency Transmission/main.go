a1c3682f54fafc55451454cd1fe3b5199e683089
Johan Idstam <johan@idstam.com>
2019-12-22 08:33:16 +0100

    2019 17 2 Done

---
+++ 2019/go/16.1 Flawed Frequency Transmission/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	input := "12345678"
+	digits := StringToSlice(input)
+	nums := StringToIntArray(digits)
+	pattern := []int{0, 1, 0, -1}
+
+	for i := 0; i < len(nums); i++ {
+
+	}
+}
